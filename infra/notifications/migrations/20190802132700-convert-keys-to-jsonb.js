dd730eb9387a46fc9404bdc556c73ff7012fdb57
Tom Linton <tom@originprotocol.com>
2019-08-02 13:55:38 +1200

    Prettier

--- infra/notifications/migrations/20190802132700-convert-keys-to-jsonb.js
+++ infra/notifications/migrations/20190802132700-convert-keys-to-jsonb.js
@@ -1,16 +1,18 @@
 'use strict'
 
 module.exports = {
-  up: (queryInterface) => {
+  up: queryInterface => {
     return queryInterface.sequelize
       .query(
         'ALTER TABLE push_subscription ALTER COLUMN keys TYPE JSONB USING CAST(keys as JSONB);'
       )
       .then(() => {
-        return queryInterface.sequelize.query('DROP EXTENSION IF EXISTS hstore;')
+        return queryInterface.sequelize.query(
+          'DROP EXTENSION IF EXISTS hstore;'
+        )
       })
   },
-  down: (queryInterface) => {
+  down: queryInterface => {
     return queryInterface.sequelize
       .query('CREATE EXTENSION IF NOT EXISTS hstore;')
       .then(() => {
